2ed84c19ce63dbae52675a742f61506229ad6b7c
rango <nneopane@oswego.edu>
2019-04-08 19:59:59 -0400

    bs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker();
