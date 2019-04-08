38f34f7924164d66f1ff636412719f21c008c185
rango <nneopane@oswego.edu>
2019-04-08 19:56:03 -0400

    rm srv wrkr

--- src/index.js
+++ src/index.js
@@ -6,4 +6,3 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App /></Router>, document.getElementById('root'));
-serviceWorker();
