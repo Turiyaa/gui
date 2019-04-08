24b0288dadda8aa017e774cc8ab7aaa0488da4d3
rango <nneopane@oswego.edu>
2019-04-08 19:01:08 -0400

    Updates

---
+++ precache-manifest.fb5ef6eabe969992d1810dc78c39bfad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "613fdbf04a00184f424f04d87db61380",
+    "url": "/gui/static/media/smartron-logo.613fdbf0.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gui/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a453f81d4aac839624262e0b7b612bbd",
+    "url": "/gui/static/media/alert.a453f81d.png"
+  },
+  {
+    "revision": "7ebcf8a4dd2565bd0c98",
+    "url": "/gui/static/js/runtime~main.7ebcf8a4.js"
+  },
+  {
+    "revision": "f003c42a7f69c65e6f3a",
+    "url": "/gui/static/js/main.f003c42a.chunk.js"
+  },
+  {
+    "revision": "9cd29e2b1a222a344c7c",
+    "url": "/gui/static/js/2.9cd29e2b.chunk.js"
+  },
+  {
+    "revision": "f003c42a7f69c65e6f3a",
+    "url": "/gui/static/css/main.34d9189a.chunk.css"
+  },
+  {
+    "revision": "fde5506e28d56554482cc1bf872f06ca",
+    "url": "/gui/index.html"
+  }
+];
